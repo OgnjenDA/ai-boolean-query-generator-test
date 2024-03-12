dfca93ef91e314412825c16bfbccd8bad200a496
Ognjen <52662230+OgnjenDA@users.noreply.github.com>
2024-03-12 11:19:12 +0100

    new model

--- main.py
+++ main.py
@@ -20,9 +20,36 @@ system_instruction = """
                       Construct Boolean queries with precision by following these structured guidelines. Your output should exclusively be a Boolean query, free from any additional descriptions or extraneous text.
 
 Query Construction Format:
-Job Titles and Variations: Begin with a list of job titles relevant to the prompt, including variations. Separate these titles using "OR" to encompass a broad range of potential matches.
-Related Skills and Expertise: Follow the job titles with a list of related skills, including both those directly mentioned in the prompt and those generally associated with the job titles and also skills related to the industry, if the industry is being mentioned in the original prompt. Use "AND" or "OR" to connect these skills, aiming to balance specificity with breadth.
-Additional Criteria: Incorporate any other relevant criteria mentioned in the prompt, such as location, language skills, previous positions, and experience. Detail these elements in sequence, prioritizing their order based on the prompt's emphasis. Also, do not add location and gender if they are not explicitly mentioned in the prompt
+Job Titles and Variations:
+Start with a compilation of job titles extracted from the user's prompt. Include any variations of these titles to ensure a comprehensive search scope.
+Utilize "OR" between different titles to cover a wide array of relevant positions.
+
+Related Skills and Expertise:
+Directly after listing job titles, enumerate skills that are:
+Explicitly mentioned in the user's prompt.
+Generally associated with the job titles.
+Related to the industry specified in the prompt, if any, this is very important.
+Connect these skills using "AND" or "OR," depending on the context, to match candidates with a broad or specific set of abilities.
+Critical: Ensure no requested skill from the prompt is omitted. Every skill the user specifies must be included in the query, especially industry related ones.
+
+Additional Criteria:
+Integrate any further relevant details specified in the prompt, focusing on elements like:
+Location: Specify cities only. Do not include countries. Use the city names exactly as mentioned by the user and dont use IN if there is no location.
+Languages: Incorporate language requirements as mentioned, using "SPEAKS" for language proficiency.
+Previous Positions and Experience: Include these details following the order and specificity provided in the prompt.
+Important: Only add criteria like location, gender, "AS NOT" exclusions, and contact information if they are explicitly mentioned by the user.
+For all other keywords (e.g., "CHANGE_PROBABILITY," "HAS EMAIL"), do not include them unless directly referenced in the user's request.
+
+Skill vs. Language Differentiation:
+Pay careful attention to differentiate between language proficiency and technical or job-related skills. For instance:
+"Proficient in English" should be categorized under languages, using "SPEAKS English."
+"Proficient in Java" identifies a technical skill and should be included in the skill list.
+
+Using AND/OR:
+Use them in skills and job titles, but never with combining other keywprds.
+incorrect example:PREVIOUSLY_AT Microsoft HAS EMAIL AND CHANGE_PROBABILITY ABOVE 75
+correct example: PREVIOUSLY_AT Microsoft HAS EMAIL CHANGE_PROBABILITY ABOVE 75
+
 Keywords and Filters:
 Current Job Title: "AS" (e.g., "AS Developer"). Exclude with "NOT" (e.g., "AS NOT Senior").
 Previous Job Title: "PREVIOUSLY_AS" for past titles. Exclude with "NOT" (e.g., "PREVIOUSLY_AS NOT Manager").
@@ -35,7 +62,7 @@ Professional Experience: "YEARS_WORKING" to specify range (e.g., "YEARS_WORKING 
 Position Tenure: "YEARS_IN_JOB" for current role duration.
 Contact Info: "HAS EMAIL" and "HAS PHONE" for availability.
 Professional Groups: Include or exclude with "IS" or "IS NOT" (e.g., "IS FREELANCER", "IS NOT STUDENT").
-Wildcards: Use "" for partial matches (e.g., "Manag").
+Wildcards: Use "*" for partial matches (e.g., "Manag").
 Term Relevance: Enhance with "^" (e.g., "Java^4 OR J2EE").
 Important Notes:
 Strict Adherence: Follow the structure and keyword usage precisely.
@@ -71,7 +98,7 @@ def display_login_form():
                 
 def display_main_app():
     st.title('AI Boolean Query Generator')
-    model_options = ["ft:gpt-3.5-turbo-1106:personal:v3:8xxaj6yT", "gpt-4-0125-preview"]
+    model_options = ["ft:gpt-3.5-turbo-1106:talentwunder::91cYiy12", "gpt-4-0125-preview"]
     selected_model = st.selectbox("Select the model:", model_options)
     user_input = st.text_area("Enter your prompt:", height=150)
 
